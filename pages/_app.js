79af0b18dcfa9c498078a7efcc4a5db990b75b0f
samir5636 <zianisamir5636789@gmail.com>
2024-10-22 22:30:51 +0100

    portfolio v2

--- pages/_app.js
+++ pages/_app.js
@@ -23,12 +23,12 @@ export default function App({ Component, pageProps }) {
 		}
 	}, [loading]);
 
-	const title = "Portfolio | Shivam Sharma";
+	const title = "Portfolio | Samir Ziani";
 	const description =
-		"Hello! I'm Shivam Sharma, a passionate web developer and problem solver. Welcome to my portfolio";
+		"Hello! I'm Samir Ziani, a passionate web and mobile developer and problem solver. Welcome to my portfolio";
 	const avatar =
-		"https://res.cloudinary.com/dyle3hnpw/image/upload/v1696091629/portfolio/WhatsApp_Image_2023-09-30_at_10.02.55_PM_jk44v9.jpg";
-	const url = "https://shivam-sharma-myportfolio.vercel.app/";
+		"";
+	const url = "";
 
 	return (
 		<>
